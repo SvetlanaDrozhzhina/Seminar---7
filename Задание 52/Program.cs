f673798d2d654a02707ad49a778a0941d36fed40
Дрожжина Светлана <arianna-14@mail.ru>
2023-04-26 11:01:57 +0300

    Добавлена задача 52

---
+++ Задание 52/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+for (int a = 0; a < numbers.GetLength(1); a++)
+{
+    double avarage = 0;
+    for (int b = 0; b < numbers.GetLength(0); b++)
+    {
+        avarage = (avarage + numbers[a,b]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            array[a, b] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        Console.Write("[ ");
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
